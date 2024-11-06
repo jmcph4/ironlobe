63ca4d372cdf60ea32aef8823e525c2d83f86d28
jmcph4 <jack@jmcph4.dev>
2024-11-06 10:57:34 +1000

    Add basic example

---
+++ examples/basic.rs
@@ -0,0 +1,30 @@
+use std::io::{self, BufRead};
+
+use ironlobe::{
+    book::{btree_book::BTreeBook, Book},
+    order::PlainOrder,
+};
+
+fn main() -> eyre::Result<()> {
+    let stdin = io::stdin();
+
+    let mut book: BTreeBook<PlainOrder> =
+        BTreeBook::new(1, "Basic".to_string(), "BAS".to_string());
+    let mut received_order = None;
+
+    while let Some(Ok(line)) = stdin.lock().lines().next() {
+        if line.trim() == "exit" {
+            break;
+        }
+
+        match serde_json::from_str(&line) {
+            Ok(order) => received_order = Some(book.add(order)?),
+            Err(e) => println!("Malformed order JSON: {e:?}"),
+        }
+
+        println!("{:?}", received_order);
+        println!("{:?}", book.top());
+    }
+
+    Ok(())
+}
