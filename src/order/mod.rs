cff25ce6f6162994495097a1a7a030d9384cdee3
jmcph4 <jack@jmcph4.dev>
2024-11-06 14:37:25 +1000

    Avoid module inception

--- src/order/mod.rs
+++ src/order/mod.rs
@@ -1,5 +1,28 @@
-pub mod order;
-pub mod plain;
+use std::fmt::Debug;
 
-pub use order::*;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+use crate::common::{Price, Quantity};
+
+pub mod plain;
 pub use plain::*;
+
+pub type OrderId = u128;
+
+#[derive(Copy, Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub enum OrderKind {
+    Bid,
+    Ask,
+}
+
+pub trait Order: Clone + Debug + Eq + PartialEq {
+    fn id(&self) -> OrderId;
+    fn kind(&self) -> OrderKind;
+    fn price(&self) -> Price;
+    fn quantity(&self) -> Quantity;
+    fn created_at(&self) -> DateTime<Utc>;
+    fn modified_at(&self) -> DateTime<Utc>;
+    fn cancelled_at(&self) -> Option<DateTime<Utc>>;
+    fn cancelled(&self) -> bool;
+}
